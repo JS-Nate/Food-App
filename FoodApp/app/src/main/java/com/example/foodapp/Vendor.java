51b82c243eb1264065de7112f994fcb2c6967ce9
Nathanael <nathanael.selvaraj@ontariotechu.net>
2023-11-20 17:49:52 -0500

    "vendor getters and setters"

--- FoodApp/app/src/main/java/com/example/foodapp/Vendor.java
+++ FoodApp/app/src/main/java/com/example/foodapp/Vendor.java
@@ -1,10 +1,60 @@
 package com.example.foodapp;
 
 public class Vendor {
+
     public Integer id;
+
     public String name;
     public String description;
     public double longitude;
     public double latitude;
     public String contact;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
