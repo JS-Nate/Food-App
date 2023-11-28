b334f9686f6aa9db1b729f66b0553dbd5283d3f0
Darren-C26 <decons2602@gmail.com>
2023-11-28 13:50:49 -0500

    Update build.gradle.kts

--- FoodApp/app/build.gradle.kts
+++ FoodApp/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("com.getkeepsafe.taptargetview:taptargetview:1.13.3")
 
 
     constraints {
